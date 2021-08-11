ca818b771dfa4c1a9c65c62b285d7fcf16c5a233
Luiza <luizafcampos@gmail.com>
2021-08-11 19:09:42 +0200

    done

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 // Setting up route
 const listItemRoute = require("./routes/listItemRoute");
